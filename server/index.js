6427af19655e617a9697a8b19cd27068977b0398
30sweetener09 <vuongan3467@gmail.com>
2025-12-21 09:24:18 +0700

    style: responsive chat, devops: deploy fe vercel, be google cloud

--- server/index.js
+++ server/index.js
@@ -7,7 +7,8 @@ const path = require("path");
 const crypto = require("crypto");
 
 try {
-  const serviceAccount = require("./firebase-admin.json");
+  const serviceAccount =
+    process.env.GOOGLE_APPLICATION_CREDENTIALS || "./firebase-admin.json";
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });
